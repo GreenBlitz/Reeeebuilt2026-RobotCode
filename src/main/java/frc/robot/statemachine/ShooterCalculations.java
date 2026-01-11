92682e8f04364550040982f59bf0086df15464b8
tomer-hershman <lili.hershman@gmail.com>
2026-01-11 19:12:16 +0200

    tomer- removed spaces

--- src/main/java/frc/robot/statemachine/ShooterCalculations.java
+++ src/main/java/frc/robot/statemachine/ShooterCalculations.java
@@ -3,7 +3,6 @@ package frc.robot.statemachine;
 import edu.wpi.first.math.geometry.Pose2d;
 import frc.robot.SimulationManager;
 
-
 public class ShooterCalculations {
 
 	public static Pose2d getTurretPose(Pose2d robotPose) {
