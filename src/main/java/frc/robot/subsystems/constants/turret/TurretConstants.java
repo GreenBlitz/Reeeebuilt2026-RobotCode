1deb56bd52037ea6b2977295d8a6f36452037cbc
itamaroryan <itamar.oryan@gmail.com>
2026-01-14 21:52:41 +0200

    nitay - some calibs

--- src/main/java/frc/robot/subsystems/constants/turret/TurretConstants.java
+++ src/main/java/frc/robot/subsystems/constants/turret/TurretConstants.java
@@ -48,7 +48,7 @@ public class TurretConstants {
 	public static final Rotation2d FORWARD_SOFTWARE_LIMIT = Rotation2d.fromDegrees(45);
 	public static final Rotation2d BACKWARDS_SOFTWARE_LIMIT = Rotation2d.fromDegrees(-150);
 	public static final Rotation2d MIN_POSITION = Rotation2d.fromDegrees(-183);
-	public static final Rotation2d MAX_POSITION = Rotation2d.fromDegrees(50);
+	public static final Rotation2d MAX_POSITION = Rotation2d.fromDegrees(177);
 	public static final Translation3d TURRET_POSITION_RELATIVE_TO_ROBOT = new Translation3d(0.25, 0, 0);
 	public static final Rotation2d MIDDLE_OF_SHOOTING_RANGE = Rotation2d
 		.fromDegrees((MAX_POSITION.getDegrees() + MIN_POSITION.getDegrees()) / 2);
