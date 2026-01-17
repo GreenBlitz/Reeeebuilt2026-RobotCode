de974b581365e711bfe49688af648c8307f41864
YHerm <0549405422.yh@gmail.com>
2026-01-17 18:08:36 +0200

    fix gr

--- src/main/java/frc/robot/subsystems/constants/omni/OmniConstant.java
+++ src/main/java/frc/robot/subsystems/constants/omni/OmniConstant.java
@@ -5,7 +5,7 @@ import frc.robot.RobotConstants;
 public class OmniConstant {
 
 	public static final String LOG_PATH = RobotConstants.SUBSYSTEM_LOGPATH_PREFIX + "/Omni";
-	public static final double GEAR_RATIO = 1 / 4.0;
+	public static final double GEAR_RATIO = 4.0;
 	public static final int CURRENT_LIMIT = 20;
 	public static final double MOMENT_OF_INERTIA = 0.001;
 	public static final boolean IS_INVERTED = false;
