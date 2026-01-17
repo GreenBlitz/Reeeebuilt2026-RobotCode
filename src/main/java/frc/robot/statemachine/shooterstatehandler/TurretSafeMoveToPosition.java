4769cbf834e5dc3c78af06d6093dfb8cdb324e14
YHerm <0549405422.yh@gmail.com>
2026-01-17 20:27:10 +0200

    format

--- src/main/java/frc/robot/statemachine/shooterstatehandler/TurretSafeMoveToPosition.java
+++ src/main/java/frc/robot/statemachine/shooterstatehandler/TurretSafeMoveToPosition.java
@@ -11,32 +11,30 @@ import java.util.function.Supplier;
 
 public class TurretSafeMoveToPosition extends Command {
 
-    private final Arm turret;
-    private final String logPath;
-    private final Supplier<Rotation2d> targetPosition;
+	private final Arm turret;
+	private final String logPath;
+	private final Supplier<Rotation2d> targetPosition;
 
+	public TurretSafeMoveToPosition(Arm turret, Supplier<Rotation2d> targetPosition, String logPath) {
+		this.turret = turret;
+		this.targetPosition = targetPosition;
+		this.logPath = logPath;
+	}
 
-    public TurretSafeMoveToPosition(Arm turret, Supplier<Rotation2d> targetPosition, String logPath) {
-        this.turret = turret;
-        this.targetPosition = targetPosition;
-        this.logPath = logPath;
-    }
+	@Override
+	public void execute() {
+		Rotation2d targetAngle = targetPosition.get();
 
-    @Override
-    public void execute() {
-        Rotation2d targetAngle = targetPosition.get();
+		if (ShooterCalculations.isTurretMoveLegal(targetAngle, turret.getPosition())) {
+			Logger.recordOutput(logPath + "/IsTurretGoingToPosition", true);
+		} else {
+			targetAngle = turret.getPosition().getDegrees() < TurretConstants.MIDDLE_OF_SHOOTING_RANGE.getDegrees()
+				? TurretConstants.BACKWARDS_SOFTWARE_LIMIT
+				: TurretConstants.FORWARD_SOFTWARE_LIMIT;
+			Logger.recordOutput(logPath + "/IsTurretGoingToPosition", false);
+		}
 
-        if (ShooterCalculations.isTurretMoveLegal(targetAngle, turret.getPosition())) {
-            Logger.recordOutput(logPath + "/IsTurretGoingToPosition", true);
-        } else {
-            targetAngle = turret.getPosition().getDegrees() < TurretConstants.MIDDLE_OF_SHOOTING_RANGE.getDegrees()
-                ? TurretConstants.BACKWARDS_SOFTWARE_LIMIT
-                : TurretConstants.FORWARD_SOFTWARE_LIMIT;
-            Logger.recordOutput(logPath + "/IsTurretGoingToPosition", false);
-        }
-
-        turret.setTargetPosition(targetAngle);
-    }
-
+		turret.setTargetPosition(targetAngle);
+	}
 
 }
