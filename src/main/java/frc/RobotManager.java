f91330612a641c79863ce208ffb9205541a42006
dolev <dolev.yahli@gmail.com>
2026-01-18 20:15:45 +0200

    did my own cr

--- src/main/java/frc/RobotManager.java
+++ src/main/java/frc/RobotManager.java
@@ -21,7 +21,6 @@ import frc.utils.time.TimeUtil;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
 
-
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to each mode, as described in the TimedRobot
  * documentation. If you change the name of this class or the package after creating this project, you must also update the build.gradle file in
