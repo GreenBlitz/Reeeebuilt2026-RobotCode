d08d4f10c4a1064c5628200825cbad6d7d8a9b92
spiderh0ok <rafaelkeselman@gmail.com>
2026-01-21 19:31:33 +0200

    most of code

--- src/main/java/frc/robot/statemachine/ShootingCalculations.java
+++ src/main/java/frc/robot/statemachine/ShootingCalculations.java
@@ -3,6 +3,7 @@ package frc.robot.statemachine;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.interpolation.Interpolator;
 import edu.wpi.first.math.interpolation.InverseInterpolator;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import frc.constants.field.Field;
@@ -19,10 +20,11 @@ public class ShootingCalculations {
 
 	private static final String LOG_PATH = "ShootingCalculations";
 	private static ShootingParams shootingParams = new ShootingParams(
-		new Rotation2d(),
-		HoodConstants.MINIMUM_POSITION,
-		TurretConstants.MIN_POSITION,
-		new Rotation2d()
+			new Rotation2d(),
+			HoodConstants.MINIMUM_POSITION,
+			TurretConstants.MIN_POSITION,
+			new Rotation2d(),
+			new Translation2d()
 	);
 
 	public static ShootingParams getShootingParams() {
@@ -30,9 +32,9 @@ public class ShootingCalculations {
 	}
 
 	public static ShootingParams calculateShootingParams(
-		Pose2d robotPose,
-		ChassisSpeeds speedsFieldRelative,
-		Rotation2d gyroYawAngularVelocity
+			Pose2d robotPose,
+			ChassisSpeeds speedsFieldRelative,
+			Rotation2d gyroYawAngularVelocity
 	) {
 		// Calculate distance from turret to target
 		Translation2d hubTranslation = Field.getHubMiddle();
@@ -41,51 +43,50 @@ public class ShootingCalculations {
 
 		// Split Robot's Speeds
 		Translation2d robotTranslationalVelocity = new Translation2d(
-			speedsFieldRelative.vxMetersPerSecond,
-			speedsFieldRelative.vyMetersPerSecond
+				speedsFieldRelative.vxMetersPerSecond,
+				speedsFieldRelative.vyMetersPerSecond
 		);
 
 		// Turret Field Relative Velocity
 		Translation2d turretTangentialVelocity = TurretConstants.TURRET_POSITION_RELATIVE_TO_ROBOT.toTranslation2d()
-			.rotateBy(Rotation2d.kCCW_90deg)
-			.times(gyroYawAngularVelocity.getRadians())
-			.rotateBy(robotPose.getRotation());
+				.rotateBy(Rotation2d.kCCW_90deg)
+				.times(gyroYawAngularVelocity.getRadians())
+				.rotateBy(robotPose.getRotation());
 		Translation2d turretFieldRelativeVelocity = robotTranslationalVelocity.plus(turretTangentialVelocity);
 
-		// TODO: add prediction to pose...
+		Translation2d turretPredictedPose = getPredictedTurretPose(turretFieldRelativeTranslation,turretFieldRelativeVelocity, turretToHubDistanceMeters);
 
-		Rotation2d angleToTarget = hubTranslation.minus(turretFieldRelativeTranslation).getAngle();
+		Rotation2d angleToTarget = hubTranslation.minus(turretPredictedPose).getAngle();
 
 		// Turret FeedForward
 		Translation2d targetRelativeTurretVelocity = turretFieldRelativeVelocity.rotateBy(angleToTarget.unaryMinus());
 		Rotation2d targetTurretVelocityCausedByTranslation = Rotation2d
-			.fromRadians(-targetRelativeTurretVelocity.getY() / turretToHubDistanceMeters);
+				.fromRadians(-targetRelativeTurretVelocity.getY() / turretToHubDistanceMeters);
 		Rotation2d turretTargetVelocityRPS = Rotation2d
-			.fromRadians(targetTurretVelocityCausedByTranslation.getRadians() - gyroYawAngularVelocity.getRadians());
+				.fromRadians(targetTurretVelocityCausedByTranslation.getRadians() - gyroYawAngularVelocity.getRadians());
 
+		double distanceFromTurretPredictedPoseToHub = getDistanceFromHub(turretPredictedPose);
 		Rotation2d turretTargetPosition = angleToTarget.minus(robotPose.getRotation());
-		Rotation2d hoodTargetPosition = hoodInterpolation(turretToHubDistanceMeters);
-		Rotation2d flywheelTargetRPS = flywheelInterpolation(turretToHubDistanceMeters);
+		Rotation2d hoodTargetPosition = hoodInterpolation(distanceFromTurretPredictedPoseToHub);
+		Rotation2d flywheelTargetRPS = flywheelInterpolation(distanceFromTurretPredictedPoseToHub);
 
 		Logger.recordOutput(LOG_PATH + "/turretTarget", turretTargetPosition);
 		Logger.recordOutput(LOG_PATH + "/turretTargetVelocityRPS", turretTargetVelocityRPS);
 		Logger.recordOutput(LOG_PATH + "/hoodTarget", hoodTargetPosition);
 		Logger.recordOutput(LOG_PATH + "/flywheelTarget", flywheelTargetRPS);
-		return new ShootingParams(flywheelTargetRPS, hoodTargetPosition, turretTargetPosition, turretTargetVelocityRPS);
+		Logger.recordOutput(LOG_PATH+ "/predictedTurretPose",
+				new Pose2d(turretPredictedPose,new Rotation2d())
+		);
+		return new ShootingParams(flywheelTargetRPS, hoodTargetPosition, turretTargetPosition, turretTargetVelocityRPS,turretPredictedPose);
 	}
 
 	private static Translation2d getFieldRelativeTurretPosition(Pose2d robotPose) {
 		Translation2d turretPositionRelativeToRobotRelativeToField = TurretConstants.TURRET_POSITION_RELATIVE_TO_ROBOT.toTranslation2d()
-			.rotateBy(robotPose.getRotation());
+				.rotateBy(robotPose.getRotation());
 		return new Translation2d(
-			robotPose.getX() + turretPositionRelativeToRobotRelativeToField.getX(),
-			robotPose.getY() + turretPositionRelativeToRobotRelativeToField.getY()
+				robotPose.getX() + turretPositionRelativeToRobotRelativeToField.getX(),
+				robotPose.getY() + turretPositionRelativeToRobotRelativeToField.getY()
 		);
-	}
-
-	private static Rotation2d getRobotRelativeLookAtHubAngleForTurret(Pose2d robotPose) {
-		Translation2d fieldRelativeTurretPose = getFieldRelativeTurretPosition(robotPose);
-		return FieldMath.getRelativeTranslation(fieldRelativeTurretPose, Field.getHubMiddle()).getAngle().minus(robotPose.getRotation());
 	}
 
 	public static double getDistanceFromHub(Translation2d pose) {
@@ -93,33 +94,33 @@ public class ShootingCalculations {
 	}
 
 	private static final InterpolationMap<Double, Rotation2d> HOOD_INTERPOLATION_MAP = new InterpolationMap<Double, Rotation2d>(
-		InverseInterpolator.forDouble(),
-		InterpolationMap.interpolatorForRotation2d(),
-		Map.of(
-			0.8,
-			Rotation2d.fromDegrees(67),
-			1.5,
-			Rotation2d.fromDegrees(60),
-			2.5,
-			Rotation2d.fromDegrees(43),
-			3.7,
-			Rotation2d.fromDegrees(33)
-		)
+			InverseInterpolator.forDouble(),
+			InterpolationMap.interpolatorForRotation2d(),
+			Map.of(
+					0.8,
+					Rotation2d.fromDegrees(67),
+					1.5,
+					Rotation2d.fromDegrees(60),
+					2.5,
+					Rotation2d.fromDegrees(43),
+					3.7,
+					Rotation2d.fromDegrees(33)
+			)
 	);
 
 	private static final InterpolationMap<Double, Rotation2d> FLYWHEEL_INTERPOLATION_MAP = new InterpolationMap<Double, Rotation2d>(
-		InverseInterpolator.forDouble(),
-		InterpolationMap.interpolatorForRotation2d(),
-		Map.of(
-			0.8,
-			Rotation2d.fromDegrees(7000),
-			1.5,
-			Rotation2d.fromDegrees(7800),
-			2.5,
-			Rotation2d.fromDegrees(10000),
-			3.7,
-			Rotation2d.fromDegrees(12000)
-		)
+			InverseInterpolator.forDouble(),
+			InterpolationMap.interpolatorForRotation2d(),
+			Map.of(
+					0.8,
+					Rotation2d.fromDegrees(7000),
+					1.5,
+					Rotation2d.fromDegrees(7800),
+					2.5,
+					Rotation2d.fromDegrees(10000),
+					3.7,
+					Rotation2d.fromDegrees(12000)
+			)
 	);
 
 	public static Rotation2d hoodInterpolation(double distanceFromTower) {
@@ -128,6 +129,26 @@ public class ShootingCalculations {
 
 	public static Rotation2d flywheelInterpolation(double distanceFromTower) {
 		return FLYWHEEL_INTERPOLATION_MAP.get(distanceFromTower);
+	}
+
+	private static final InterpolationMap<Double, Double> DISTANCE_TO_BALL_FLIGHT_TIME_INTERPOLATION_MAP = new InterpolationMap<Double, Double>(
+			InverseInterpolator.forDouble(),
+			Interpolator.forDouble(),
+			Map.of(
+					0.1,
+					0.2,
+					0.5,
+					0.4
+			)
+	);
+
+	public static Translation2d getPredictedTurretPose(Translation2d turretPose, Translation2d turretVelocities, double distanceFromHubMeters) {
+		double ballFlightTime = DISTANCE_TO_BALL_FLIGHT_TIME_INTERPOLATION_MAP.get(distanceFromHubMeters);
+
+		double turretPosePredictionX = turretPose.getX() + (turretVelocities.getX() * ballFlightTime);
+		double turretPosePredictionY = turretPose.getY() + (turretVelocities.getY() * ballFlightTime);
+
+		return new Translation2d(turretPosePredictionX,turretPosePredictionY);
 	}
 
 	public static void updateShootingParams(Pose2d robotPose, ChassisSpeeds speedsFieldRelative, Rotation2d gyroYawAngularVelocity) {
