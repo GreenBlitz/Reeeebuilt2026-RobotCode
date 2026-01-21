6cd6eee18c54899597f0e5f11142569913ac6b64
DanielleCarmi09 <daniellecarmi@gmail.com>
2026-01-21 15:46:47 +0200

    compile

--- src/main/java/frc/robot/subsystems/swerve/Swerve.java
+++ src/main/java/frc/robot/subsystems/swerve/Swerve.java
@@ -192,6 +192,8 @@ public class Swerve extends GBSubsystem {
 			odometryData[i] = new OdometryData(
 				imuSignals.yawSignal().getTimestamps()[i],
 				modules.getWheelPositions(i),
+				imu instanceof EmptyIMU ? Optional.empty() : Optional.of(imuSignals.rollSignal().asArray()[i]),
+				imu instanceof EmptyIMU ? Optional.empty() : Optional.of(imuSignals.pitchSignal().asArray()[i]),
 				imu instanceof EmptyIMU ? Optional.empty() : Optional.of(imuSignals.yawSignal().asArray()[i]),
 				imu instanceof EmptyIMU ? Optional.empty() : Optional.of(getIMUAcceleration())
 			);
