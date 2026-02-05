991830c229f6de8f31b43a4c353c2475c3bdf45c
spiderh0ok <rafaelkeselman@gmail.com>
2026-02-05 20:52:39 +0200

    for someReason works

--- src/main/java/frc/robot/autonomous/AutosBuilder.java
+++ src/main/java/frc/robot/autonomous/AutosBuilder.java
@@ -28,18 +28,35 @@ public class AutosBuilder {
 	private static Supplier<PathPlannerAutoWrapper> getRightFirstQuarterAuto(Robot robot) {
 		return () -> new PathPlannerAutoWrapper(
 				new SequentialCommandGroup(
-					new ParallelDeadlineGroup(
-							PathFollowingCommandsBuilder.commandDuringPath(robot.getSwerve(),() -> robot.getPoseEstimator().getEstimatedPose(),PathHelper.PATH_PLANNER_PATHS.get("Right start 1-1"),DEFAULT_PATH_CONSTRAINS,() -> new ParallelCommandGroup(
-									robot.getRobotCommander().getShooterStateHandler().setState(ShooterState.RESET_SUBSYSTEMS),
-									robot.getRobotCommander().getIntakeStateHandler().setState(IntakeState.RESET_FOUR_BAR)
-							),new Pose2d(0.3,0.3,Rotation2d.fromDegrees(5))),
-							new WaitUntilCommand(() -> robot.getPoseEstimator().getEstimatedPose().getX() > 6 && robot.getRobotCommander().getIntakeStateHandler().hasFourBarBeenReset()).andThen(robot.getRobotCommander().getIntakeStateHandler().setState(IntakeState.INTAKE).asProxy())),
-						PathFollowingCommandsBuilder.commandDuringPath(robot.getSwerve(),() -> robot.getPoseEstimator().getEstimatedPose(), PathHelper.PATH_PLANNER_PATHS.get("Right 1-2"), DEFAULT_PATH_CONSTRAINS,() -> robot.getRobotCommander().scoreSequence(),new Pose2d(0.1,0.1,Rotation2d.fromDegrees(3))
-					)),
+						new ParallelDeadlineGroup(
+								PathFollowingCommandsBuilder.followPath(
+										PathHelper.PATH_PLANNER_PATHS.get("Right start 1-1")),
+										new ParallelCommandGroup(
+												robot.getRobotCommander()
+														.getShooterStateHandler()
+														.setState(ShooterState.RESET_SUBSYSTEMS),
+												robot.getRobotCommander()
+														.getIntakeStateHandler()
+														.setState(IntakeState.RESET_FOUR_BAR)
+										).withInterruptBehavior(Command.InterruptionBehavior.kCancelIncoming).andThen(
+												robot.getRobotCommander()
+														.getIntakeStateHandler()
+														.setState(IntakeState.INTAKE)
+										)),
+						PathFollowingCommandsBuilder.commandDuringPath(
+								robot.getSwerve(),
+								() -> robot.getPoseEstimator().getEstimatedPose(),
+								PathHelper.PATH_PLANNER_PATHS.get("Right 1-2"),
+								DEFAULT_PATH_CONSTRAINS,
+								() -> robot.getRobotCommander().scoreSequence(),
+								new Pose2d(0.1, 0.1, Rotation2d.fromDegrees(3))
+						)
+				),
 				new Pose2d(),
 				"Right path type 1"
-			);
+		);
 	}
+
 
 	public static List<Supplier<PathPlannerAutoWrapper>> getAutoList(Robot robot) {
 		return List.of(getRightFirstQuarterAuto(robot));
