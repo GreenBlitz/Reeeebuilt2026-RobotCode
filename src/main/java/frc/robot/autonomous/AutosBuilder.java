00974c5c6900c9b018568e5574ed965c222a54fc
spiderh0ok <rafaelkeselman@gmail.com>
2026-02-08 18:18:36 +0200

    hada

--- src/main/java/frc/robot/autonomous/AutosBuilder.java
+++ src/main/java/frc/robot/autonomous/AutosBuilder.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.Robot;
 import frc.robot.statemachine.intakestatehandler.IntakeState;
 import frc.robot.statemachine.shooterstatehandler.ShooterState;
+import frc.robot.subsystems.swerve.Swerve;
 import frc.utils.auto.PathHelper;
 import frc.utils.auto.PathPlannerAutoWrapper;
 import java.util.List;
@@ -21,65 +22,64 @@ public class AutosBuilder {
 		);
 	}
 
-	private static Supplier<PathPlannerAutoWrapper> getRightFirstQuarterAuto(Robot robot) {
+	public static List<Supplier<PathPlannerAutoWrapper>> getAutoList(Robot robot,Supplier<Command> intake,Supplier<Command> scoreSequence) {
+		return List.of(getRightFirstQuarterAuto(robot,scoreSequence,intake), getLeftFirstQuarterAuto(robot,scoreSequence,intake));
+	}
+
+	private static Supplier<PathPlannerAutoWrapper> getRightFirstQuarterAuto(Robot robot,Supplier<Command> scoreSequence, Supplier<Command> intake) {
 		return () -> new PathPlannerAutoWrapper(
 			new SequentialCommandGroup(
-				firstQuarterAutoStart(robot, false),
-				defaultDeadlineCommandWithPath(
-					robot,
-					PathHelper.PATH_PLANNER_PATHS.get("R mid - Outpost"),
-					() -> robot.getRobotCommander().scoreSequence()
-				)
+				rightStartingLineToRightMiddleWith(robot, intake,false),
+				rightMiddleToOutpostWithScoring(robot,scoreSequence)
 			),
 			new Pose2d(),
 			"R starting - R mid - Outpost"
 		);
 	}
 
-	private static Supplier<PathPlannerAutoWrapper> getLeftFirstQuarterAuto(Robot robot) {
+	private static Supplier<PathPlannerAutoWrapper> getLeftFirstQuarterAuto(Robot robot,Supplier<Command> scoreSequence,Supplier<Command> intake) {
 		return () -> new PathPlannerAutoWrapper(
 			new SequentialCommandGroup(
-				firstQuarterAutoStart(robot, true),
-				defaultDeadlineCommandWithPath(
-					robot,
-					PathHelper.PATH_PLANNER_PATHS.get("L mid - Depot"),
-					() -> robot.getRobotCommander().scoreSequence()
-				)
+				rightStartingLineToRightMiddleWith(robot, intake,true),
+				leftMiddleToDepotWithScoring(robot,scoreSequence)
 			),
 			new Pose2d(),
 			"L starting - L mid - Depot"
 		);
 	}
 
-	public static List<Supplier<PathPlannerAutoWrapper>> getAutoList(Robot robot) {
-		return List.of(getRightFirstQuarterAuto(robot), getLeftFirstQuarterAuto(robot));
-	}
-
-	private static Command resetSubsystemsAndIntakeAfter(Robot robot) {
-		return new ParallelCommandGroup(
-			robot.getRobotCommander().getShooterStateHandler().setState(ShooterState.RESET_SUBSYSTEMS),
-			robot.getRobotCommander().getIntakeStateHandler().setState(IntakeState.RESET_FOUR_BAR)
-		).andThen(robot.getRobotCommander().getIntakeStateHandler().setState(IntakeState.INTAKE));
-	}
-
-	private static Command firstQuarterAutoStart(Robot robot, boolean isPathLeft) {
-		return defaultDeadlineCommandWithPath(
-			robot,
-			isPathLeft
+	private static Command rightStartingLineToRightMiddleWith(Robot robot, Supplier<Command> intake, boolean isMirrored) {
+		return PathFollowingCommandsBuilder.deadlineCommandWithPath(
+				robot.getSwerve(),
+				() -> robot.getPoseEstimator().getEstimatedPose(),
+		isMirrored
 				? PathHelper.PATH_PLANNER_PATHS.get("R starting - R mid").mirrorPath()
 				: PathHelper.PATH_PLANNER_PATHS.get("R starting - R mid"),
-			() -> resetSubsystemsAndIntakeAfter(robot)
+				AutonomousConstants.DEFAULT_PATH_CONSTRAINS,
+				intake,
+				AutonomousConstants.DEFAULT_PATH_TOLERANCE
 		);
 	}
 
-	private static Command defaultDeadlineCommandWithPath(Robot robot, PathPlannerPath deadline, Supplier<Command> command) {
+	private static Command rightMiddleToOutpostWithScoring(Robot robot, Supplier<Command> scoreSequence) {
 		return PathFollowingCommandsBuilder.deadlineCommandWithPath(
-			robot.getSwerve(),
-			() -> robot.getPoseEstimator().getEstimatedPose(),
-			deadline,
-			AutonomousConstants.DEFAULT_PATH_CONSTRAINS,
-			command,
-			AutonomousConstants.DEFAULT_PATH_TOLERANCE
+				robot.getSwerve(),
+				() -> robot.getPoseEstimator().getEstimatedPose(),
+				PathHelper.PATH_PLANNER_PATHS.get("R mid - Outpost"),
+				AutonomousConstants.DEFAULT_PATH_CONSTRAINS,
+				scoreSequence,
+				AutonomousConstants.DEFAULT_PATH_TOLERANCE
+		);
+	}
+
+	private static Command leftMiddleToDepotWithScoring(Robot robot, Supplier<Command> scoreSequence) {
+		return PathFollowingCommandsBuilder.deadlineCommandWithPath(
+				robot.getSwerve(),
+				() -> robot.getPoseEstimator().getEstimatedPose(),
+				PathHelper.PATH_PLANNER_PATHS.get("L mid - Depot"),
+				AutonomousConstants.DEFAULT_PATH_CONSTRAINS,
+				scoreSequence,
+				AutonomousConstants.DEFAULT_PATH_TOLERANCE
 		);
 	}
 
