e5abb8d3fb9388e867465ad6bb0f92ca15ad2e8b
DanielleCarmi09 <daniellecarmi@gmail.com>
2026-02-11 18:21:55 +0200

    shooting pose test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -52,6 +52,8 @@ import frc.utils.battery.BatteryUtil;
 import frc.utils.brakestate.BrakeStateManager;
 import frc.utils.math.StandardDeviations2D;
 
+import java.util.Optional;
+
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a "declarative" paradigm, very little robot logic should
  * actually be handled in the {@link RobotManager} periodic methods (other than the scheduler calls). Instead, the structure of the robot
--- src/main/java/frc/robot/vision/cameras/limelight/LimelightFilters.java
+++ src/main/java/frc/robot/vision/cameras/limelight/LimelightFilters.java
@@ -6,6 +6,7 @@ import frc.constants.field.Field;
 import frc.robot.vision.DetectedObjectType;
 import frc.utils.Filter;
 import frc.utils.math.ToleranceMath;
+import org.littletonrobotics.junction.Logger;
 
 import java.util.Arrays;
 import java.util.Optional;
@@ -29,7 +30,7 @@ public class LimelightFilters {
 		return MegaTagFilters.isRobotInField(() -> limelight.getMT1RawData().pose().getTranslation(), robotInFieldTolerance)
 			.and(
 				MegaTagFilters.doesYawExistAtTimestamp(timestamp, wantedYawAtTimestamp)
-					.and(
+					.implies(
 						MegaTagFilters.isYawAtExpectedAngle(
 							() -> limelight.getMT1RawData().pose().getRotation(),
 							() -> wantedYawAtTimestamp.apply(timestamp).get(),
@@ -87,8 +88,12 @@ public class LimelightFilters {
 			Supplier<Boolean> isExpectedYawCalibrated,
 			Rotation2d expectedYawTolerance
 		) {
-			return () -> !isExpectedYawCalibrated.get()
-				|| ToleranceMath.isNearWrapped(expectedYawSupplier.get(), cameraSuppliedRobotYaw.get(), expectedYawTolerance);
+			return () -> {
+				Logger.recordOutput("Expected", expectedYawSupplier.get());
+				Logger.recordOutput("Supplied", cameraSuppliedRobotYaw.get());
+				return !isExpectedYawCalibrated.get()
+					|| ToleranceMath.isNearWrapped(expectedYawSupplier.get(), cameraSuppliedRobotYaw.get(), expectedYawTolerance);
+			};
 		}
 
 		private static Filter isYawNotZero(Supplier<Rotation2d> robotYaw) {
