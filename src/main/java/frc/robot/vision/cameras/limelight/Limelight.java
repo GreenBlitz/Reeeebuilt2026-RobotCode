dcb266c9a399b9f360a085067bb946ac08301565
daniel <danielshapir1@gmail.com>
2026-02-12 14:40:14 +0200

    save before removal of comments

--- src/main/java/frc/robot/vision/cameras/limelight/Limelight.java
+++ src/main/java/frc/robot/vision/cameras/limelight/Limelight.java
@@ -212,13 +212,15 @@ public class Limelight implements ObjectDetector, IndependentRobotPoseSupplier, 
 		return new ArrayList<>();
 	}
 
-	public Pose2d getRobotPoseFromTurretLimeLight(Supplier<Transform2d> camTransformRelativeToRobotCenter, String cameraName) {
+	public static Pose2d getRobotPoseFromTurretLimeLight(Pose2d limeLightPoseRelativeToRobotCenter, Pose2d limeLightPoseRelativeToField) {
 		// important remember that the pose the limelight helpers return are shifted by the given pose in the constructor
 		// to avoid shiftinge at the constructor use LimelightHelpers.setCameraPose_RobotSpace when pose is set to zero
 		// what transform 2d does is set the angle to 0 like a new axis were working on
-
-		Pose2d limeLightPoseRelativeToField = LimelightHelpers.getBotPose3d_wpiBlue(cameraName).toPose2d();
-		return (limeLightPoseRelativeToField.transformBy(camTransformRelativeToRobotCenter.get().inverse()));
+		Transform2d camTransformRelativeToRobotCenter = new Transform2d(
+			new Pose2d(0, 0, Rotation2d.fromDegrees(0.0)),
+			limeLightPoseRelativeToRobotCenter
+		);
+		return (limeLightPoseRelativeToField.transformBy(camTransformRelativeToRobotCenter.inverse()));
 	}
 
 	public Function<LimelightHelpers.RawDetection, Boolean> getNeuralDetectionFilter() {
