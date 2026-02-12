429f6961084ed30ee4de860cd8df719f24dd6691
dolev <dolev.yahli@gmail.com>
2026-02-12 23:52:08 +0200

    i wanna go to the fll competition😭😭😭😭😭😭

--- src/main/java/frc/robot/statemachine/ShootingCalculations.java
+++ src/main/java/frc/robot/statemachine/ShootingCalculations.java
@@ -274,7 +274,7 @@ public class ShootingCalculations {
 	}
 
 	public static void updateShootingParams(Pose2d robotPose, ChassisSpeeds speedsFieldRelative, Rotation2d gyroYawAngularVelocity) {
-		if (ShootingChecks.isInAllianceZone(getFieldRelativeTurretPosition(robotPose), LOG_PATH + "/isInAllianceZone")) {
+		if (ShootingChecks.isInAllianceZone(getFieldRelativeTurretPosition(robotPose), LOG_PATH)) {
 			shootingParams = calculateScoringParams(robotPose, speedsFieldRelative, gyroYawAngularVelocity);
 		} else {
 			shootingParams = calculatePassingParams(robotPose, speedsFieldRelative, gyroYawAngularVelocity);
