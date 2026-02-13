bdec75195d6b1e5e9fbf0064101613a1d1dee9da
Nitay4 <nitay@rotbart.co.il>
2026-02-13 21:08:44 +0200

    nitay - did

--- src/main/java/frc/robot/IDs.java
+++ src/main/java/frc/robot/IDs.java
@@ -21,13 +21,13 @@ public class IDs {
 		public final static Phoenix6DeviceID FLYWHEEL = new Phoenix6DeviceID(10, BusChain.ROBORIO);
 		public final static Phoenix6DeviceID FLYWHEEL_FOLLOWER = new Phoenix6DeviceID(11, BusChain.ROBORIO);
 
-		public final static Phoenix6DeviceID FOUR_BAR = new Phoenix6DeviceID(12, BusChain.ROBORIO);
+		public static final Phoenix6DeviceID FOUR_BAR = new Phoenix6DeviceID(12, BusChain.ROBORIO);
 
-		public final static Phoenix6DeviceID HOOD = new Phoenix6DeviceID(20, BusChain.ROBORIO);
+		public static final Phoenix6DeviceID HOOD = new Phoenix6DeviceID(20, BusChain.ROBORIO);
 
 		public static final Phoenix6DeviceID TURRET = new Phoenix6DeviceID(30, BusChain.ROBORIO);
 
-		public final static Phoenix6DeviceID TRAIN = new Phoenix6DeviceID(40, BusChain.ROBORIO);
+		public static final Phoenix6DeviceID TRAIN = new Phoenix6DeviceID(40, BusChain.ROBORIO);
 
 	}
 
@@ -54,21 +54,21 @@ public class IDs {
 
 	public static class SparkMAXIDs {
 
-		public final static SparkMaxDeviceID INTAKE_ROLLERS = new SparkMaxDeviceID(6, SparkLowLevel.MotorType.kBrushless);
+		public static final SparkMaxDeviceID INTAKE_ROLLERS = new SparkMaxDeviceID(6, SparkLowLevel.MotorType.kBrushless);
 
-		public final static SparkMaxDeviceID BELLY = new SparkMaxDeviceID(60, SparkLowLevel.MotorType.kBrushless);
+		public static final SparkMaxDeviceID BELLY = new SparkMaxDeviceID(60, SparkLowLevel.MotorType.kBrushless);
 
 	}
 
 	public static class DigitalInputsIDs {
 
-		public final static int FOUR_BAR_RESET_SENSOR = 11;
+		public static final int FOUR_BAR_RESET_SENSOR = 11;
 
-		public final static int TURRET_RESET_SENSOR = 12;
+		public static final int TURRET_RESET_SENSOR = 12;
 
-		public final static int HOOD_RESET_SENSOR = 7;
+		public static final int HOOD_RESET_SENSOR = 7;
 
-		public final static int TRAIN_BALL_SENSOR = 9;
+		public static final int TRAIN_BALL_SENSOR = 9;
 
 	}
 
