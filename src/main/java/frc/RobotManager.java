51c77d2770a109a298c2f79c36d5e4402ceef9fb
maya <maya.romm0401@gmail.com>
2026-02-19 17:42:26 +0200

    .

--- src/main/java/frc/RobotManager.java
+++ src/main/java/frc/RobotManager.java
@@ -7,8 +7,11 @@ package frc;
 import com.revrobotics.util.StatusLogger;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Threads;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Robot;
+import frc.robot.autonomous.AutonomousConstants;
 import frc.utils.HubUtil;
 import frc.utils.driverstation.DriverStationUtil;
 import frc.utils.alerts.AlertManager;
@@ -108,8 +111,18 @@ public class RobotManager extends LoggedRobot {
 	}
 
 	private void createAutoReadyForConstructionChooser() {
-		this.autonomousCommand = robot.getAutonomousCommand();
-		BrakeStateManager.brake();
+		SendableChooser<Boolean> autoReadyForConstructionSendableChooser = new SendableChooser<>();
+		autoReadyForConstructionSendableChooser.setDefaultOption("false", false);
+		autoReadyForConstructionSendableChooser.addOption("true", true);
+		autoReadyForConstructionSendableChooser.onChange(isReady -> {
+			if (isReady) {
+				BrakeStateManager.brake();
+			} else {
+				BrakeStateManager.coast();
+			}
+			Logger.recordOutput(AutonomousConstants.LOG_PATH_PREFIX + "/ReadyToConstruct", isReady);
+		});
+		SmartDashboard.putData("AutoReadyForConstruction", autoReadyForConstructionSendableChooser);
 	}
 
 	private void updateTimeRelatedData() {
