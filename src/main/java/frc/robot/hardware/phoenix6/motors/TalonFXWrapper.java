82fa3d1576468b518b3093ee5043e131d7990b5c
itamaroryan <itamar.oryan@gmail.com>
2026-02-27 01:10:21 +0200

    code

--- src/main/java/frc/robot/hardware/phoenix6/motors/TalonFXWrapper.java
+++ src/main/java/frc/robot/hardware/phoenix6/motors/TalonFXWrapper.java
@@ -9,7 +9,11 @@ import frc.robot.hardware.phoenix6.Phoenix6Util;
 public class TalonFXWrapper extends TalonFX {
 
 	private static final int DEFAULT_CONFIG_NUMBER_OF_TRIES = 1;
-
+	
+	public TalonFXWrapper(int deviceId) {
+		this(new Phoenix6DeviceID(deviceId));
+	}
+	
 	public TalonFXWrapper(Phoenix6DeviceID ctreDeviceID) {
 		super(ctreDeviceID.id(), ctreDeviceID.busChain().getCANBus());
 	}
