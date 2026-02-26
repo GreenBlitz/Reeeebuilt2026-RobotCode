f02171b3fde5fc46a0a2865633a105608b7de793
itamaroryan <itamar.oryan@gmail.com>
2026-02-26 23:11:14 +0200

    code

--- src/main/java/frc/robot/statemachine/funnelstatehandler/FunnelStateHandler.java
+++ src/main/java/frc/robot/statemachine/funnelstatehandler/FunnelStateHandler.java
@@ -59,9 +59,9 @@ public class FunnelStateHandler {
 	private Command rollUntilSensor() {
 		return new SequentialCommandGroup(
 			new ParallelCommandGroup(
-				train.getCommandsBuilder().setVelocity(FunnelState.ROLL_UNTIL_SENSOR.getTrainVelocity()),
+//				train.getCommandsBuilder().setVelocity(FunnelState.ROLL_UNTIL_SENSOR.getTrainVelocity()),
 				belly.getCommandsBuilder().setVoltage(FunnelState.ROLL_UNTIL_SENSOR.getBellyVoltage())
-			).until(this::isBallAtSensor),
+			),
 			new ParallelCommandGroup(train.getCommandsBuilder().stop(), belly.getCommandsBuilder().stop())
 		);
 	}
