7b59b902617e7296d803cdcec8bb123a9473d3bb
daniel <danielshapir1@gmail.com>
2026-03-03 16:42:24 +0200

    changes

--- src/main/java/frc/robot/odometry/OdometryPlus.java
+++ src/main/java/frc/robot/odometry/OdometryPlus.java
@@ -11,10 +11,10 @@ import java.util.function.Supplier;
 
 public class OdometryPlus<T> extends Odometry<T> {
 
-	private final Supplier<Rotation3d> robotAnglesSupplier;
+	private final Supplier<Rotation3d> robotOrientationSupplier;
 
 	/**
-	 * Constructs an Odometry object.
+	 * Constructs a 2d odometry object that takes into consideration the robot's 3d orientation.
 	 *
 	 * @param kinematics        The kinematics of the drivebase.
 	 * @param gyroAngle         The angle reported by the gyroscope.
@@ -23,7 +23,7 @@ public class OdometryPlus<T> extends Odometry<T> {
 	 */
 	public OdometryPlus(Kinematics<?, T> kinematics, Rotation2d gyroAngle, T wheelPositions, Pose2d initialPoseMeters) {
 		super(kinematics, gyroAngle, wheelPositions, initialPoseMeters);
-		robotAnglesSupplier = null;
+		robotOrientationSupplier = null;
 	}
 
 	public OdometryPlus(
@@ -31,31 +31,32 @@ public class OdometryPlus<T> extends Odometry<T> {
 		Rotation2d gyroAngle,
 		T wheelPositions,
 		Pose2d initialPoseMeters,
-		Supplier<Rotation3d> robotAnglesSupplier
+		Supplier<Rotation3d> robotOrientationSupplier
 	) {
 		super(kinematics, gyroAngle, wheelPositions, initialPoseMeters);
-		this.robotAnglesSupplier = robotAnglesSupplier;
+		this.robotOrientationSupplier = robotOrientationSupplier;
 	}
 
 	@Override
 	public Pose2d update(Rotation2d gyroAngle, T wheelPositions) {
 		Pose2d lastPose = getPoseMeters();
-		Pose2d rawPose = super.update(gyroAngle, wheelPositions);
+		Pose2d unCompensatedPose = super.update(gyroAngle, wheelPositions);
 
-		if (robotAnglesSupplier == null)
-			return rawPose;
+		if (robotOrientationSupplier == null)
+			return unCompensatedPose;
 
-		Rotation3d angles = robotAnglesSupplier.get();
+		Rotation3d robotOrientation = robotOrientationSupplier.get();
 		// in case angle above 90 so wont minus , i dont think pose is relevant when robot flips
-		double scale = Math.max(0, Math.cos(angles.rotateBy(new Rotation3d(0, 0, -angles.getZ())).getY()));
+		double scale = Math.max(0, Math.cos(robotOrientation.rotateBy(new Rotation3d(0, 0, -robotOrientation.getZ())).getY()));
 
-		Transform2d delta = new Transform2d(lastPose, rawPose);
+		Transform2d unCompensatedDistance = new Transform2d(lastPose, unCompensatedPose);
 
-		Pose2d corrected = lastPose.plus(new Transform2d(delta.getTranslation().times(scale), delta.getRotation()));
+		Pose2d compensatedByPitch = lastPose
+			.plus(new Transform2d(unCompensatedDistance.getTranslation().times(scale), unCompensatedDistance.getRotation()));
 
-		resetTranslation(corrected.getTranslation());
+		resetTranslation(compensatedByPitch.getTranslation());
 
-		return corrected;
+		return compensatedByPitch;
 	}
 
 }
