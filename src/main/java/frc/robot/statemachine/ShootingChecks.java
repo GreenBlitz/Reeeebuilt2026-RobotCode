055f3b48c1d6959499ed6f02016362091f93452c
YHerm <0549405422.yh@gmail.com>
2026-03-03 19:34:37 +0200

    reverts

--- src/main/java/frc/robot/statemachine/ShootingChecks.java
+++ src/main/java/frc/robot/statemachine/ShootingChecks.java
@@ -236,7 +236,7 @@ public class ShootingChecks {
 		);
 		boolean isInAllianceZone = isInAllianceZone(robot.getPoseEstimator().getEstimatedPose().getTranslation());
 
-		return isReadyToShoot /* && isOurHubReadyToStartShooting && isInAllianceZone */;
+		return isReadyToShoot && isOurHubReadyToStartShooting && isInAllianceZone;
 	}
 
 	public static boolean isReadyToPass(
@@ -285,8 +285,7 @@ public class ShootingChecks {
 
 		boolean isInAllianceZone = isInAllianceZone(robot.getPoseEstimator().getEstimatedPose().getTranslation());
 
-		return canContinueShooting;
-		// && isOurHubReadyToStartShooting && isInAllianceZone;
+		return canContinueShooting && isOurHubReadyToStartShooting && isInAllianceZone;
 	}
 
 	public static boolean canContinuePassing(
