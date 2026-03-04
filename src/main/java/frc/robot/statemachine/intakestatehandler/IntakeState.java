ded5eb255d9e8cead8bcf54913a7aa25034ed61a
maya <maya.romm0401@gmail.com>
2026-03-04 17:53:20 +0200

    cr

--- src/main/java/frc/robot/statemachine/intakestatehandler/IntakeState.java
+++ src/main/java/frc/robot/statemachine/intakestatehandler/IntakeState.java
@@ -7,9 +7,9 @@ public enum IntakeState {
 	CLOSED(Rotation2d.fromDegrees(90), 0),
 	INTAKE(Rotation2d.fromDegrees(20), 0.8),
 	OUTTAKE(Rotation2d.fromDegrees(20), -0.8),
-	RESET_FOUR_BAR(Rotation2d.fromRotations(Double.NaN), Double.NaN),
-	CALIBRATION(Rotation2d.fromRotations(Double.NaN), Double.NaN),
-	STAY_IN_PLACE(Rotation2d.fromRotations(Double.NaN), Double.NaN);
+	RESET_FOUR_BAR,
+	CALIBRATION,
+	STAY_IN_PLACE;
 
 	private final Rotation2d fourBarPosition;
 	private final double intakePower;
