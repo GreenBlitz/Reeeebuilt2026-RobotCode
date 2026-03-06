9ce30e84e3212b5b7c58fbfc2ee0460402aa7f36
Nitay4 <nitay@rotbart.co.il>
2026-03-06 17:06:28 +0200

    nitay - did

--- src/main/java/frc/robot/statemachine/ShootingChecks.java
+++ src/main/java/frc/robot/statemachine/ShootingChecks.java
@@ -285,7 +285,7 @@ public class ShootingChecks {
 
 		boolean isInAllianceZone = isInAllianceZone(robot.getPoseEstimator().getEstimatedPose().getTranslation());
 
-		return canContinueShooting /*&& isOurHubReadyToStartShooting && isInAllianceZone*/;
+		return canContinueShooting && isOurHubReadyToStartShooting && isInAllianceZone;
 	}
 
 	public static boolean canContinuePassing(
