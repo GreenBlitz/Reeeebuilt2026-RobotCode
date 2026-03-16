1b4fcd2e1f99c8c56d3dfd97942fd1ac4f6d6cdd
tomer-hershman <lili.hershman@gmail.com>
2026-03-16 17:21:07 +0200

    did the job

--- src/main/java/frc/robot/autonomous/AutosBuilder.java
+++ src/main/java/frc/robot/autonomous/AutosBuilder.java
@@ -74,45 +74,64 @@ public class AutosBuilder {
 		double stuckDebounceSeconds,
 		AllianceSide startingSide
 	) {
+		hasPathEnded = false;
 		return () -> new PathPlannerAutoWrapper(
-			new ParallelCommandGroup(
-				PathFollowingCommandsBuilder
-					.followAdjustedPathThenStop(
-						robot.getSwerve(),
-						() -> robot.getPoseEstimator().getEstimatedPose(),
-						startingSide == AllianceSide.DEPOT
-							? PathHelper.PATH_PLANNER_PATHS.get("L quarter")
-							: PathHelper.PATH_PLANNER_PATHS.get("R quarter"),
-						pathfindingConstraints,
-						regularIsNearEndOfPathTolerance,
-						stuckIsNearEndOfPathTolerance,
-						stuckDebounceSeconds,
-						robot.getSwerve().getLogPath()
-
-					)
-					.asProxy()
-					.alongWith(new InstantCommand(() -> hasPathEnded = false))
-					.andThen(new InstantCommand(() -> hasPathEnded = true)),
-				new SequentialCommandGroup(
-					resetSubsystems.get(),
-					new ParallelCommandGroup(
-						new WaitCommand(AutonomousConstants.TIME_TO_WAIT_TO_START_SHOOTING_AFTER_AUTO_START).andThen(scoreSequence.get()),
-						openIntake.get()
-							.until(() -> hasPathEnded)
-							.andThen(
-								new ParallelCommandGroup(
-									new WaitCommand(AutonomousConstants.TIME_TO_WAIT_TO_START_WIGGLE_AFTER_PATH_END)
-										.andThen(
-											robot.getSwerve()
-												.getCommandsBuilder()
-												.wiggle(AutonomousConstants.WIGGLE_RANGE, AutonomousConstants.TIME_BETWEEN_WIGGLES_SECONDS)
-										)
-										.asProxy(),
-									new WaitCommand(AutonomousConstants.TIME_TO_WAIT_TO_CLOSE_INTAKE_AFTER_PATH_END_SECONDS)
-										.andThen(closeIntake.get())
-								)
+			new SequentialCommandGroup(
+				new ParallelDeadlineGroup(
+					new SequentialCommandGroup(
+						new WaitUntilCommand(() -> hasPathEnded),
+						new WaitCommand(AutonomousConstants.TIME_TO_EMPTY_TANK_AFTER_END_OF_PATH)
+					),
+					new SequentialCommandGroup(
+						PathFollowingCommandsBuilder
+							.followAdjustedPathThenStop(
+								robot.getSwerve(),
+								() -> robot.getPoseEstimator().getEstimatedPose(),
+								startingSide == AllianceSide.DEPOT
+									? PathHelper.PATH_PLANNER_PATHS.get("L quarter")
+									: PathHelper.PATH_PLANNER_PATHS.get("R quarter"),
+								pathfindingConstraints,
+								regularIsNearEndOfPathTolerance,
+								stuckIsNearEndOfPathTolerance,
+								stuckDebounceSeconds,
+								robot.getSwerve().getLogPath()
 							)
+							.asProxy()
+							.alongWith(new InstantCommand(() -> hasPathEnded = false))
+							.andThen(new InstantCommand(() -> hasPathEnded = true)),
+						new SequentialCommandGroup(
+							resetSubsystems.get(),
+							new ParallelDeadlineGroup(
+								new WaitCommand(AutonomousConstants.TIME_TO_WAIT_TO_START_SHOOTING_AFTER_AUTO_START)
+									.andThen(scoreSequence.get()),
+								openIntake.get()
+									.until(() -> hasPathEnded)
+									.andThen(
+										new ParallelCommandGroup(
+											new WaitCommand(AutonomousConstants.TIME_TO_WAIT_TO_START_WIGGLE_AFTER_PATH_END).andThen(
+												robot.getSwerve()
+													.getCommandsBuilder()
+													.wiggle(AutonomousConstants.WIGGLE_RANGE, AutonomousConstants.TIME_BETWEEN_WIGGLES_SECONDS)
+											).asProxy(),
+											new WaitCommand(AutonomousConstants.TIME_TO_WAIT_TO_CLOSE_INTAKE_AFTER_PATH_END_SECONDS)
+												.andThen(closeIntake.get())
+										)
+									)
+							)
+						)
 					)
+				),
+				doubleSwipeCommand(
+					robot,
+					resetSubsystems,
+					openIntake,
+					closeIntake,
+					scoreSequence,
+					pathfindingConstraints,
+					regularIsNearEndOfPathTolerance,
+					stuckIsNearEndOfPathTolerance,
+					stuckDebounceSeconds,
+					startingSide
 				)
 			),
 			new Pose2d(),
@@ -120,5 +139,58 @@ public class AutosBuilder {
 		);
 	}
 
+	private static Command doubleSwipeCommand(
+		Robot robot,
+		Supplier<Command> resetSubsystems,
+		Supplier<Command> openIntake,
+		Supplier<Command> closeIntake,
+		Supplier<Command> scoreSequence,
+		PathConstraints pathfindingConstraints,
+		Pose2d regularIsNearEndOfPathTolerance,
+		Pose2d stuckIsNearEndOfPathTolerance,
+		double stuckDebounceSeconds,
+		AllianceSide allianceSide
+	) {
+		return new ParallelCommandGroup(
+			PathFollowingCommandsBuilder
+				.followAdjustedPathThenStop(
+					robot.getSwerve(),
+					() -> robot.getPoseEstimator().getEstimatedPose(),
+					allianceSide == AllianceSide.DEPOT
+						? PathHelper.PATH_PLANNER_PATHS.get("L double swipe")
+						: PathHelper.PATH_PLANNER_PATHS.get("R double swipe"),
+					pathfindingConstraints,
+					regularIsNearEndOfPathTolerance,
+					stuckIsNearEndOfPathTolerance,
+					stuckDebounceSeconds,
+					robot.getSwerve().getLogPath()
+
+				)
+				.asProxy()
+				.alongWith(new InstantCommand(() -> hasPathEnded = false))
+				.andThen(new InstantCommand(() -> hasPathEnded = true)),
+			new SequentialCommandGroup(
+				resetSubsystems.get(),
+				new ParallelCommandGroup(
+					new WaitCommand(AutonomousConstants.TIME_TO_WAIT_TO_START_SHOOTING_AFTER_AUTO_START).andThen(scoreSequence.get()),
+					openIntake.get()
+						.until(() -> hasPathEnded)
+						.andThen(
+							new ParallelCommandGroup(
+								new WaitCommand(AutonomousConstants.TIME_TO_WAIT_TO_START_WIGGLE_AFTER_PATH_END)
+									.andThen(
+										robot.getSwerve()
+											.getCommandsBuilder()
+											.wiggle(AutonomousConstants.WIGGLE_RANGE, AutonomousConstants.TIME_BETWEEN_WIGGLES_SECONDS)
+									)
+									.asProxy(),
+								new WaitCommand(AutonomousConstants.TIME_TO_WAIT_TO_CLOSE_INTAKE_AFTER_PATH_END_SECONDS)
+									.andThen(closeIntake.get())
+							)
+						)
+				)
+			)
+		);
+	}
 
 }
