c17d0985d73ee089151971feeaa44f37b0fb87e6
Nitay4 <nitay@rotbart.co.il>
2026-03-24 18:32:11 +0200

    nitay - did

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -15,6 +15,6 @@ public class RobotConstants {
 	public static final double WHEEL_COF = 1;
 
 	public static final double MAX_TIME_FOR_BPS_INTERPOLATOR = 35;
-	public static final double TIME_FOR_AVERAGE_BPS_CALCULATION_SECONDS = 5;
+	public static final double TIME_FOR_AVERAGE_BPS_CALCULATION_SECONDS = 1;
 
 }
