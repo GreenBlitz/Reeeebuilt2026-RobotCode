500af8748b159cff441b84389096429ed24f1bcf
DANNA2828 <dan71967@gmail.com>
2026-04-02 23:06:10 +0300

    danna - added new stddevs

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -149,9 +149,9 @@ public class Robot {
 		limelightFront.setMT1StdDevsCalculation(
 			LimelightStdDevCalculations.getMT1StdDevsCalculation(
 				limelightFront,
+				new StandardDeviations2D(0.5),
+				new StandardDeviations2D(0.15),
 				new StandardDeviations2D(0.4),
-				new StandardDeviations2D(0.07),
-				new StandardDeviations2D(0.7),
 				new StandardDeviations2D(0.011)
 			)
 		);
@@ -177,9 +177,9 @@ public class Robot {
 		limelightRight.setMT1StdDevsCalculation(
 			LimelightStdDevCalculations.getMT1StdDevsCalculation(
 				limelightRight,
+				new StandardDeviations2D(0.5),
+				new StandardDeviations2D(0.15),
 				new StandardDeviations2D(0.4),
-				new StandardDeviations2D(0.07),
-				new StandardDeviations2D(0.7),
 				new StandardDeviations2D(0.011)
 			)
 		);
@@ -205,9 +205,9 @@ public class Robot {
 		limelightLeft.setMT1StdDevsCalculation(
 			LimelightStdDevCalculations.getMT1StdDevsCalculation(
 				limelightLeft,
+				new StandardDeviations2D(0.5),
+				new StandardDeviations2D(0.15),
 				new StandardDeviations2D(0.4),
-				new StandardDeviations2D(0.07),
-				new StandardDeviations2D(0.7),
 				new StandardDeviations2D(0.011)
 			)
 		);
