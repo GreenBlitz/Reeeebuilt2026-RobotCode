17040fe13c781d0514d0c671e8d7411b98558535
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2026-04-08 23:23:19 +0300

    fourbat better

--- src/main/java/frc/robot/statemachine/intakestatehandler/IntakeStateHandler.java
+++ src/main/java/frc/robot/statemachine/intakestatehandler/IntakeStateHandler.java
@@ -92,23 +92,22 @@ public class IntakeStateHandler {
 							.setVoltageWithoutLimit(
 								FourBarConstants.CLOSE_VOLTAGE,
 								() -> fourBar.getCurrent() > FourBarConstants.COLLISION_STALL_CURRENT
-							)
+							),
+							fourBar.getCommandsBuilder()
+									.setCurrentWithoutLimit(FourBarConstants.RELAXED_CLOSE_CURRENT_AMP)
+									.withTimeout(FourBarConstants.RELAXED_CLOSE_TIME_SECONDS)
 					),
 
 					rollers.getCommandsBuilder().setPower(IntakeState.INTAKE.getIntakePower())
 				),
 				new ParallelCommandGroup(
-					new SequentialCommandGroup(
-						fourBar.getCommandsBuilder()
-							.setCurrentWithoutLimit(FourBarConstants.RELAXED_CLOSE_CURRENT_AMP)
-							.withTimeout(FourBarConstants.RELAXED_CLOSE_TIME_SECONDS),
+
 						fourBar.getCommandsBuilder()
 							.setCurrentWithoutLimit(
 								() -> isCloseFourBarHarder.getAsBoolean()
 									? FourBarConstants.RELAXED_CLOSE_CURRENT_AMP
 									: FourBarConstants.HOLD_CLOSE_CURRENT_AMP
-							)
-					),
+							),
 					rollers.getCommandsBuilder().setPower(IntakeState.CLOSED.getIntakePower())
 				)
 			)
