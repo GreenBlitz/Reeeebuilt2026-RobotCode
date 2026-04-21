a675b734c1e88ef621544f0426e48e214938c8dc
rys4590 <ronnie.yadid@gmail.com>
2026-04-21 17:51:48 +0300

    sA

--- src/main/java/frc/robot/vision/cameras/limelight/inputs/LimelightHardwareInputs.java
+++ src/main/java/frc/robot/vision/cameras/limelight/inputs/LimelightHardwareInputs.java
@@ -4,7 +4,7 @@ import frc.robot.hardware.ConnectedInput;
 import org.littletonrobotics.junction.AutoLog;
 
 @AutoLog
-public class LimelightHardwareInputs extends ConnectedInput{
+public class LimelightHardwareInputs extends ConnectedInput {
 
 	public double temperatureCelsius = 0.0;
 
