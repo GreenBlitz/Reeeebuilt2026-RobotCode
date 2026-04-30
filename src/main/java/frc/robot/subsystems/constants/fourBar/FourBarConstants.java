70bc278cbe3ead5a6bf127be12e935e4323d527f
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2026-04-30 17:45:44 +0300

    nitay - pit

--- src/main/java/frc/robot/subsystems/constants/fourBar/FourBarConstants.java
+++ src/main/java/frc/robot/subsystems/constants/fourBar/FourBarConstants.java
@@ -66,7 +66,7 @@ public class FourBarConstants {
 
 	public static final double HARD_OPEN_TIME_SECONDS = 1;
 
-	public static final double HARD_OPEN_CURRENT_AMP_FOR_AUTONOMOUS = -8.0;
+	public static final double HARD_OPEN_CURRENT_AMP_FOR_AUTONOMOUS = -10.0;
 
 	public static final double HARD_OPEN_TIME_SECONDS_FOR_AUTONOMOUS = 0.3;
 
