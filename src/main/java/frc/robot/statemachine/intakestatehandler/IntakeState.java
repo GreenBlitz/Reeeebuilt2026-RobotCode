3867ac4371c5ae0632ae5b3b506230dd031ad0e0
YHerm <0549405422.yh@gmail.com>
2026-06-16 14:22:49 +0300

    good code

--- src/main/java/frc/robot/statemachine/intakestatehandler/IntakeState.java
+++ src/main/java/frc/robot/statemachine/intakestatehandler/IntakeState.java
@@ -5,7 +5,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 public enum IntakeState {
 
 	CLOSED(Rotation2d.fromDegrees(90), 0.3),
-	SLOW_CLOSE(Rotation2d.fromDegrees(90), 0.3),
+	SLOW_CLOSE(Rotation2d.fromDegrees(Double.NaN), 0.3),
 	INTAKE(Rotation2d.fromDegrees(22), 0.9),
 	OUTTAKE(Rotation2d.fromDegrees(22), -0.9),
 	RESET_FOUR_BAR(Rotation2d.fromRotations(Double.NaN), Double.NaN),
