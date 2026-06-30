9b2b6c9f1c4eebd6afe3442a286e83ab81896010
Spiderh0ok <rafaelkeselman@gmail.com>
2026-06-30 22:08:01 +0300

    auto fix

--- src/main/java/frc/robot/autonomous/AutosBuilder.java
+++ src/main/java/frc/robot/autonomous/AutosBuilder.java
@@ -1195,7 +1195,7 @@ public class AutosBuilder {
 	) {
 		return new ParallelDeadlineGroup(
 			new WaitCommand(AutonomousConstants.MINIMUM_TIME_AFTER_STARTING_TO_SHOOT_TO_RETURN_TO_MIDDLE)
-				.andThen(new RunCommand(() -> {}).until(() -> hasStoppedThrowingBalls(robot))),
+				.andThen(new RunCommand(() -> {}).until(() -> hasStoppedThrowingBalls(robot))).andThen(closeIntake.get()),
 			scoreSequence.get()
 		).until(
 			() -> returnToMiddle.getAsBoolean()
@@ -1207,7 +1207,7 @@ public class AutosBuilder {
 			.andThen(
 				new ParallelCommandGroup(
 					scoreSequence.get(),
-					closeIntake.get(),
+                    closeIntake.get(),
 					PathFollowingCommandsBuilder
 						.followAdjustedPathThenStop(
 							robot.getSwerve(),
