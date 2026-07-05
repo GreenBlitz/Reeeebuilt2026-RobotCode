a04557c7c3ca5e63170761de76432e2d36ad70f7
itamaroryan <itamar.oryan@gmail.com>
2026-07-05 21:24:36 +0300

    deleted changes

--- src/main/java/frc/robot/vision/cameras/limelight/inputs/ColorDetectionInputs.java
+++ src/main/java/frc/robot/vision/cameras/limelight/inputs/ColorDetectionInputs.java
@@ -1,6 +1,7 @@
 package frc.robot.vision.cameras.limelight.inputs;
 
 import frc.robot.vision.cameras.limelight.LimelightTarget2dValues;
+import frc.utils.limelight.LimelightHelpers;
 import org.littletonrobotics.junction.AutoLog;
 
 @AutoLog
@@ -8,6 +9,6 @@ public class ColorDetectionInputs {
 
 	public LimelightTarget2dValues target2dValues = new LimelightTarget2dValues();
 
-//	public LimelightHelpers.RawTarget[] rawTargets = new LimelightHelpers.RawTarget[0];
+	public LimelightHelpers.RawTarget[] rawTargets = new LimelightHelpers.RawTarget[0];
 
 }
